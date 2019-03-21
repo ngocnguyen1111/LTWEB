0405305c27a0566f17b19bd1166e6fc71825d7ad
ngocnguyen1111 <ntmn.ngocnguyen@gmail.com>
2019-03-21 19:01:25 +0700

    ket noi db

--- myweb/Controllers/HomeController.cs
+++ myweb/Controllers/HomeController.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using myweb.Models;
 namespace myweb.Controllers
 {
     public class HomeController : Controller
     {
+        dbQLSPORTDataContext data = new dbQLSPORTDataContext();
+        private List<PRODUCT> laysanpham(int count)
+        {
+            //Sắp xếp sách theo ngày cập nhật, sau đó lấy top @count 
+            return data.PRODUCTs.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult HomePage()
         {
-            return View();
+            //Lấy top 5 Album bán chạy nhất
+            var sanphammoi = laysanpham(5);
+            return View(sanphammoi);
         }
         public ActionResult About()
         {
