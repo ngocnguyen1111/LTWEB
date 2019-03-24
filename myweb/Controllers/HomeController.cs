cc86ddaa5510ea5b1efda0ab96b6a048d361bfa8
ngocnguyen1111 <ntmn.ngocnguyen@gmail.com>
2019-03-24 18:53:40 +0700

    trang chu ok

--- myweb/Controllers/HomeController.cs
+++ myweb/Controllers/HomeController.cs
@@ -6,24 +6,23 @@ using System.Web.Mvc;
 using myweb.Models;
 namespace myweb.Controllers
 {
+    [System.Runtime.InteropServices.ComVisible(true)]
+    [System.Serializable]
     public class HomeController : Controller
     {
         dbQLSPORTDataContext data = new dbQLSPORTDataContext();
-        private List<PRODUCT> laysanpham(int count)
+        private List<PRODUCT>laysanpham(int count)
         {
             //Sắp xếp sách theo ngày cập nhật, sau đó lấy top @count 
             return data.PRODUCTs.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
         }
         public ActionResult Index()
         {
-            return View();
+            var sp = laysanpham(5);
+            return View(sp);
+           
         }
-        public ActionResult HomePage()
-        {
-            //Lấy top 5 Album bán chạy nhất
-            var sanphammoi = laysanpham(5);
-            return View(sanphammoi);
-        }
+       
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -38,6 +37,10 @@ namespace myweb.Controllers
             return View();
         }
         public ActionResult Category()
+        {
+            return View();
+        }
+        public ActionResult SingleProduct()
         {
             return View();
         }
